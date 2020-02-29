eeaee15758d85790dbd6fb05c083502bfc8b09eb
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-02-29 18:28:21 +0200

    Frontpage starting to take shape

--- front-page.php
+++ front-page.php
@@ -13,18 +13,45 @@
         </div>
         <div class="col-md-8 col-sm-12">
             <div class="row">
-                <div class="col-md-2 col-sm-0"></div>
+                <div class="col-md-2 col-sm-0">
+                </div>
                 <div class="col-md-8 col-sm-12">
                     <div class="text-center p-3">
                         <h2>Kahvila Lorem</h2>
                     </div>
                     <div class="main p-3">
-                        <p>Testi</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt arcu at odio interdum, id sollicitudin est tempor. Maecenas eu metus leo. Pellentesque eget euismod risus. Aliquam nec erat felis. Fusce et maximus lacus. Donec fermentum mauris lectus, quis vehicula justo mattis vel. Praesent mollis velit sit amet ultrices mattis. Vivamus fermentum gravida justo id facilisis. Vivamus et nisi eu arcu luctus dignissim vitae quis leo. Proin lacus lectus, interdum id lobortis et, elementum in est. Phasellus lectus enim, dapibus a metus eu, interdum mollis justo. Nullam sagittis leo augue, sed aliquam libero interdum eu. Sed et scelerisque massa, sit amet suscipit lectus. Nam vehicula tincidunt turpis.</p>
                     </div>
                 </div>
             <div class="col-md-2 col-sm-0">
             </div>
             </div>
+
+        <div class="row">
+            <div class="col-12 spacing-between-content">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-5 col-sm-12">
+                <div class="text-center">
+                    <h3>Menu</h3>
+                </div>
+                <div class="p-3">
+                    <p>Nunc sit amet mollis justo. Fusce tempor varius nisi. Aenean vel accumsan purus, non vehicula dolor. Ut cursus neque vitae pulvinar faucibus. Vestibulum hendrerit varius ipsum, ut luctus elit luctus quis. Fusce mollis elementum felis et dapibus. Donec laoreet condimentum diam mattis tempor. Nulla sed ipsum gravida, accumsan tortor quis, finibus tellus. Aenean blandit nisi a hendrerit laoreet. Sed mollis ornare sagittis. Duis ac justo dictum, aliquam ipsum ac, pulvinar lectus. Maecenas vel lacus nisl. Fusce gravida vel odio ac finibus. Nam malesuada suscipit quam, ac pellentesque velit interdum eget.</p>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-0">
+            </div>
+            <div class="col-md-5 col-sm-12">
+                <div class="text-center">
+                    <h3>Kahvila</h3>
+                </div>
+                <div class="p-3">
+                    <p>In at risus in augue tristique viverra sit amet vel arcu. Quisque eget enim vitae tellus aliquam malesuada. Sed varius ligula eget augue dignissim mollis. Vestibulum ac molestie lorem, at volutpat odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut ultrices pulvinar lectus in ultricies. Aliquam sem lacus, tincidunt id convallis quis, auctor vitae libero. Duis sit amet leo luctus, posuere dolor in, consequat diam. Morbi at varius justo. Nam sit amet fringilla libero.</p>
+                </div>
+            </div>
+        </div>
         </div>
         <div class="col-md-2 col-sm-0 spacing-sides">
         </div>
