b63d894f7182d48c848c3e38aceb0ac65bce9fb3
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-02-29 18:46:28 +0200

    Frontpage taking more shape

--- front-page.php
+++ front-page.php
@@ -38,6 +38,7 @@
                     <h3>Menu</h3>
                 </div>
                 <div class="p-3">
+                    <img src="<?php echo get_template_directory_uri();?>/img/menu.jpg" alt="Kuva menusta" class="img-main-pages img-border-1px">
                     <p>Nunc sit amet mollis justo. Fusce tempor varius nisi. Aenean vel accumsan purus, non vehicula dolor. Ut cursus neque vitae pulvinar faucibus. Vestibulum hendrerit varius ipsum, ut luctus elit luctus quis. Fusce mollis elementum felis et dapibus. Donec laoreet condimentum diam mattis tempor. Nulla sed ipsum gravida, accumsan tortor quis, finibus tellus. Aenean blandit nisi a hendrerit laoreet. Sed mollis ornare sagittis. Duis ac justo dictum, aliquam ipsum ac, pulvinar lectus. Maecenas vel lacus nisl. Fusce gravida vel odio ac finibus. Nam malesuada suscipit quam, ac pellentesque velit interdum eget.</p>
                 </div>
             </div>
@@ -48,10 +49,37 @@
                     <h3>Kahvila</h3>
                 </div>
                 <div class="p-3">
+                    <img src="<?php echo get_template_directory_uri();?>/img/exterior.jpg" alt="Kahvila ulkoa" class="img-main-pages img-border-1px">
                     <p>In at risus in augue tristique viverra sit amet vel arcu. Quisque eget enim vitae tellus aliquam malesuada. Sed varius ligula eget augue dignissim mollis. Vestibulum ac molestie lorem, at volutpat odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut ultrices pulvinar lectus in ultricies. Aliquam sem lacus, tincidunt id convallis quis, auctor vitae libero. Duis sit amet leo luctus, posuere dolor in, consequat diam. Morbi at varius justo. Nam sit amet fringilla libero.</p>
                 </div>
             </div>
         </div>
+
+            <div class="row">
+                <div class="col-12 spacing-between-content">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-2 col-sm-0">
+                </div>
+                <div class="col-md-8 col-sm-12">
+                    <div class="text-center p-3">
+                        <h2>Yhteystiedot</h2>
+                    </div>
+                    <div class="main p-3 text-center">
+                        <p>Puhelin: 3854932556</p>
+                        <p>Osoite: Jokukatu 23</p>
+                        <p>Kaupunki: Jossain</p>
+                        <p>Postinumero: 01123</p>
+                        <p>Sähköposti: kahvilalorem@loremipsum.com</p>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-0">
+                </div>
+            </div>
+
+
         </div>
         <div class="col-md-2 col-sm-0 spacing-sides">
         </div>
