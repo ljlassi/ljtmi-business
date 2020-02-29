0957c1209428aff57e9100d66a8b42248f0b0532
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-02-29 15:08:20 +0200

    Changes to frontpage

--- front-page.php
+++ front-page.php
@@ -13,29 +13,8 @@
         </div>
         <div class="col-md-8 col-sm-12">
             <div class="row">
-                <div class="col-md-7 col-sm-12">
                     <div class="main p-3">
-                        <?php
-
-                        /**
-                         * The post loop, call get the template for displaying page if there is a page to display.
-                         */
-
-
-                        if ( have_posts() ) {
-                            while ( have_posts() ) : the_post();
-                                get_template_part( 'template-parts/content/content', 'page' );
-                            endwhile;
-                        }
-                        ?>
-                    </div>
-                </div>
-                <div class="col-md-1 col-sm-0 "></div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="sidebar-right p-3">
-                        <?php if ( is_active_sidebar( 'right-side-bar' ) ) : ?>
-                            <?php dynamic_sidebar( 'right-side-bar' ); ?>
-                        <?php endif; ?>
+                        <h2>Kahvila Lorem</h2>
                     </div>
                 </div>
             </div>
